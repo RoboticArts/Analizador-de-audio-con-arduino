406a03f5aeba6078f8c5ed85a92e486c8cec0ebb
RoboticArts <roboticarts1@gmail.com>
2017-08-29 03:21:56 +0200

    Add files via upload

--- Analizador_de_Audio.ino
+++ Analizador_de_Audio.ino
@@ -1,7 +1,6 @@
 
 #include <Adafruit_GFX.h>    // Libreria de graficos
 #include <Adafruit_TFTLCD.h> // Libreria de LCD 
-#include <TouchScreen.h>     // Libreria del panel tactil
 
 
 /************************** CONFIGURACÓN DE LA PANTALLA TFT *****************/
@@ -9,19 +8,6 @@
 #define XM A2  // Pin analogico A2 para ADC
 #define YM 7 
 #define XP 6 
-
-short TS_MINX = 150; // Coordenadas del panel tactil para delimitar
-short TS_MINY = 120; // el tamaño de la zona donde podemos presionar
-short TS_MAXX = 850; // y que coincida con el tamaño del LCD
-short TS_MAXY = 891; 
-
-
-// Definimos la presion máxima y minima que podemos realizar sobre el panel
-#define MINPRESSURE 1
-#define MAXPRESSURE 1000
-
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 364); 
-
 
 
 //Es RGB 565: http://www.barth-dev.de/online/rgb565-color-picker/
@@ -43,8 +29,6 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 364);
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET); // Instancia LCD 
 
-int button_A = 12;
-int button_B = 11;
 
 /******************* CONFIGURACIÓN DEL ADC POR REGISTROS *******************/
 
@@ -59,7 +43,6 @@ int button_B = 11;
 
 
 
-
 // macros
 // http://yaab-arduino.blogspot.co.il/2015/02/fast-sampling-from-analog-input.html
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -69,10 +52,12 @@ int button_B = 11;
 
 /********************** VARIABLES DEL CÓDIGO ******************/
 
-    int randomNumber = 0;
-    int last_randomNumber = 0;
+    int button_A = 12;
+    int button_B = 11;
+
     int offsetX = 50;
     int offsetY = 10;
+    int FFT_Number = 0;
   
     //Para la aguja del tacómetro
     double circleX = 160;
@@ -101,111 +86,7 @@ void setup() {
 
 void loop() {
 
-  /*
-    tft.setRotation(0);
-
-    //25Hz
-    randomNumber = map( fht_log_out [0], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX, 10 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 10 + offsetY, randomNumber , 10, WHITE); // Dibujamos un cuadrado/rectangulo relleno de color 
-                                       //(Punto inicial X, Punto inicial Y, Longitud X,Longitud Y, Color)
-    //50Hz
-    randomNumber = map( fht_log_out [1], 0 , 256, 0 ,100); 
-    tft.fillRect(0  + offsetX, 30 + offsetY , 200, 10, BLACK);
-    tft.fillRect(0  + offsetX, 30 + offsetY , randomNumber, 10, WHITE ); 
-    
-    //100Hz
-    randomNumber = map( fht_log_out [2], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX, 50 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 50 + offsetY, randomNumber, 10, WHITE ); 
-    
-    
-    //200HZ
-    randomNumber = map( fht_log_out [3], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,70 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 70 + offsetY, randomNumber, 10, WHITE ); 
-    
-    //500hz
-    randomNumber = map( fht_log_out [4], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,90 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 90 + offsetY, randomNumber, 10, WHITE );   
-    
-    //800Hz
-    randomNumber = map( fht_log_out [5], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,110 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 110 + offsetY, randomNumber, 10, WHITE ); 
-   
-    //1KHz
-    randomNumber = map( fht_log_out [6], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,130 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 130 + offsetY, randomNumber, 10, WHITE ); 
-    
-    //1.5 Khz
-    randomNumber = map( fht_log_out [7], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,150 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 150 + offsetY, randomNumber, 10, WHITE ); 
-     
-    //2kHz
-    //randomNumber = map( fht_log_out [8], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,170 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 170 + offsetY, randomNumber, 10, WHITE ); 
-  
-    //2.5 KHz
-    randomNumber = map( fht_log_out [9], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,190 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 190 + offsetY, randomNumber, 10, WHITE );  
-    
-    //3Khz
-    randomNumber = map( fht_log_out [10], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,210 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 210 + offsetY, randomNumber, 10, WHITE ); 
-   
-    //5KHz
-    randomNumber = map( fht_log_out [11], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,230 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 230 + offsetY, randomNumber, 10 , WHITE );   
-    
-    //10Khz
-    //randomNumber = map( fht_log_out [12], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,250 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 250 + offsetY, randomNumber, 10 , WHITE ); 
-  
-    //20Khz
-    randomNumber = map( fht_log_out [13], 0 , 256, 0 ,100); 
-    tft.fillRect(0 + offsetX,270 + offsetY, 200, 10, BLACK);
-    tft.fillRect(0 + offsetX, 270 + offsetY, randomNumber, 10 , WHITE ); 
-  */
-
-
-/*
-    tft.setCursor(-30 + offsetX, 152 + offsetY);
-    tft.setTextSize(5); 
-    tft.setTextColor(WHITE); 
-    tft.fillRect(-30 + offsetX,152 + offsetY, 200, 50, BLACK);
-    tft.println(decibelios);
-  */   
-
-
-  /* 
-   for(int i = 157; i>=0; i--){
-   float alpha = float(i)/100; // Primer cuadrante
-   tft.fillTriangle(int(100*cos(alpha)+160),int(240-(100*sin(alpha)+120)),160,120,int(100*cos(alpha)+160),120,RED);
-   
-   //Cuarto cuadrante
-   tft.fillTriangle(int(100*cos(alpha)+160),int((100*sin(alpha)+120)),160,120,int(100*cos(alpha)+160),120,BLUE);
-   
-   float beta = 1.57+(1.57-alpha); //Segundo cuadrante
-   tft.fillTriangle(int(100*cos(beta)+160),int(240-(100*sin(beta)+120)),160,120,int(100*cos(beta)+160),120,YELLOW);
-   
-   //Tercer cuadrante
-   tft.fillTriangle(int(100*cos(beta)+160),int((100*sin(beta)+120)),160,120,int(100*cos(beta)+160),120,GREEN);
-   }
-   tft.fillCircle(160,120, 65, BLACK);
-   delay(10000);
-   delay(10000);
-   delay(10000);
-   */
-   
+ 
     //show_SoundMeter(3); 
     // MODOS
     // 1 -> Amplitud en %
@@ -274,16 +155,8 @@ void loop() {
       case 3: show_SpectrumFFT(1); break;
       case 4: show_SpectrumFFT(2); break;
     }
-
-    Serial.println(modos);
-
-    
-  
-
     
 } 
-
-
 
 
 float MeasureVolume(int mode)
@@ -293,12 +166,11 @@ float MeasureVolume(int mode)
   for (int i = 0; i < MicSamples; i++)
   {
     
-
-    while (!(ADCSRA & _BV(ADIF))); // wait for adc to be ready (ADIF)
-    sbi(ADCSRA, ADIF); // restart adc
-    byte m = ADCL; // fetch adc data
+    while (!(ADCSRA & _BV(ADIF))); // Espera a que el ADC esté listo (ADIF)
+    sbi(ADCSRA, ADIF); // Se resetea el ADC
+    byte m = ADCL; 
     byte j = ADCH;
-    int k = ((int)j << 8) | m; // form into an int
+    int k = ((int)j << 8) | m; // Se convierte en int
 
 
     int amp = abs(k - AmpMax);
@@ -316,7 +188,7 @@ float MeasureVolume(int mode)
   // convert from 0 to 100
   soundVolMax = 100 * soundVolMax / AmpMax; 
   soundVolRMSflt = 100 * soundVolRMSflt / AmpMax;
-  soundVolRMS = 10 * soundVolRMSflt / 7; // RMS to estimate peak (RMS is 0.7 of the peak in sin)
+  soundVolRMS = 10 * soundVolRMSflt / 7; // Calculo del RMS aproximado
 
 
   if(mode == 1)
@@ -333,10 +205,10 @@ float MeasureVolume(int mode)
 void init_fast_ADC()
 {
   
-  ADCSRA = 0xE5;
-  ADMUX = 0x5; // ADC 5 sin configurar Vref
-  DIDR0 = 0x20;
-  analogReference(EXTERNAL); // 3.3V to AREF
+  ADCSRA = 0xE5; //Prescaler de 32  // 11100101
+  ADMUX = 0x5; // ADC 5 sin configurar Vref // 00000101
+  DIDR0 = 0x20; // Se activa el ahorro de energia para el ADC 5
+  analogReference(EXTERNAL); // 3.3V de AREF
   // http://yaab-arduino.blogspot.co.il/2015/02/fast-sampling-from-analog-input.html
   // http://maxembedded.com/2011/06/the-adc-of-the-avr/
   //https://sites.google.com/site/qeewiki/books/avr-guide/analog-input
@@ -461,13 +333,7 @@ void init_SpectrumFFT(int mode){
   }
 
 void show_SoundMeter(int mode){
-  /*
-     static bool key_sound = 1; //Se inicia una vez
-     if(key_sound == 1){
-      key_sound = 0; 
-      init_SoundMeter(160,100,120); // Posicion X, Posicion Y, Radio /// Probado con R:100  X: 160 Y: 120
-     }
- */
+
     float medida = MeasureVolume(mode); // 1: Amplitud Max 2: RMS 3: Decibelios
     static float ultima_medida = 0;
     static int last_mode = 0;
@@ -535,33 +401,26 @@ void MeasureFHT(){
   
   for (int i = 0; i < FHT_N; i++) { // save 256 samples
 
-    while (!(ADCSRA & /*0x10*/_BV(ADIF))); // wait for adc to be ready (ADIF)
-    sbi(ADCSRA, ADIF); // restart adc
-    byte m = ADCL; // fetch adc data
+    while (!(ADCSRA & /*0x10*/_BV(ADIF))); // Espera a que esté preparado el ADC (ADIF)
+    sbi(ADCSRA, ADIF); // Resetea el ADC
+    byte m = ADCL; // Se obtienen los valores
     byte j = ADCH;
-    int k = ((int)j << 8) | m; // form into an int
+    int k = ((int)j << 8) | m; 
 
-    k -= 0x0200; // form into a signed int
-    k <<= 6; // form into a 16b signed int
-    //k <<= FreqGainFactorBits;
-    
-    fht_input[i] = k; // put real data into bins
+    k -= 0x0200; 
+    k <<= 6; // Se convierte a int con signo
+   
+    fht_input[i] = k; // La lectura se almacena en el vector FHT
     
   }
-  fht_window(); // window the data for better frequency response
-  fht_reorder(); // reorder the data before doing the fht
-  fht_run(); // process the data in the fht
-  fht_mag_log();  //process amplitude fht in dB
+  fht_window(); // Se aplica una ventana
+  fht_reorder(); // Se reordena los datos
+  fht_run(); // Se procesa la fht
+  fht_mag_log();  //Convierte la amplitud en dB
 }
 
 void show_SpectrumFFT(int mode){
-  /*
-  static bool key_fft = 1; //Se inicializa una unica vez
-  if(key_fft == 1){
-    init_SpectrumFFT(mode);
-    key_fft = 0;
-  }
-  */
+
   MeasureFHT();
 
  
@@ -571,9 +430,9 @@ void show_SpectrumFFT(int mode){
     for(int i = 2; i<=127; i++){ //126 bins
       int separacion = 2;
       tft.drawLine(0 + offsetX, 10+separacion*i + offsetY, 200 + offsetX , 10+separacion*i+offsetY, BLACK); //Se borra el anterior valor
-      randomNumber = fht_log_out[i]-90; //Filtro digital
-      if(randomNumber <= 0){randomNumber = 0;} 
-      tft.drawLine(0 + offsetX, 10+separacion*i + offsetY, randomNumber + offsetX , 10 + separacion*i + offsetY, WHITE);  //Se muestra el nuevo valor
+      FFT_Number = fht_log_out[i]-90; //Filtro digital
+      if(FFT_Number <= 0){FFT_Number = 0;} 
+      tft.drawLine(0 + offsetX, 10+separacion*i + offsetY, FFT_Number + offsetX , 10 + separacion*i + offsetY, WHITE);  //Se muestra el nuevo valor
       }
   }
 
@@ -583,13 +442,12 @@ void show_SpectrumFFT(int mode){
     for(int i = 2; i<=29; i++){ //28 bins
       int separacion = 9;
       tft.drawLine(0 + offsetX, 10+separacion*i + offsetY, 200 + offsetX , 10+separacion*i+offsetY, BLACK); //Se borra el anterior valor
-      randomNumber = fht_log_out[i]-90; //Filtro digital
-      if(randomNumber <= 0){randomNumber = 0;} 
-      tft.drawLine(0 + offsetX, 10+separacion*i + offsetY, randomNumber + offsetX , 10 + separacion*i + offsetY, WHITE);  //Se muestra el nuevo valor
+      FFT_Number = fht_log_out[i]-90; //Filtro digital
+      if(FFT_Number <= 0){FFT_Number = 0;} 
+      tft.drawLine(0 + offsetX, 10+separacion*i + offsetY, FFT_Number + offsetX , 10 + separacion*i + offsetY, WHITE);  //Se muestra el nuevo valor
       }
   }
   
   
 }
-  
 
